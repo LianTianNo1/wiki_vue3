b4f703781fc0af93f5c1a94fdac243620c4e6d6a
田浪 <1584731441@qq.com>
2021-03-26 15:30:38 +0800

    2021-3-26 init

---
+++ src/main/java/com/wzy/wiki/WikiApplication.java
@@ -0,0 +1,13 @@
+package com.wzy.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiApplication.class, args);
+    }
+
+}
