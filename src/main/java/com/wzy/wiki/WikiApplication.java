58bd5c59f97a4b9fade6227b132eacbea364a06b
田浪 <1584731441@qq.com>
2021-03-26 15:49:57 +0800

    2021-3-26 修改启动文案

--- src/main/java/com/wzy/wiki/WikiApplication.java
+++ src/main/java/com/wzy/wiki/WikiApplication.java
@@ -1,13 +1,23 @@
 package com.wzy.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
