c5b79cabcb32e6b0ff6ebc26da6dcf76ed4cb95a
田浪 <1584731441@qq.com>
2021-03-27 16:05:48 +0800

    2021-3-27 开发Hello World接口 访问地址: http://127.0.0.1:8080/hello

--- src/main/java/com/wzy/wiki/config/WikiApplication.java
+++ src/main/java/com/wzy/wiki/config/WikiApplication.java
@@ -1,12 +1,13 @@
-package com.wzy.wiki;
+package com.wzy.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.wzy")
 @SpringBootApplication
 public class WikiApplication {
 
