23d01da94989a511d53300a0d27a6e9a188f65f2
田浪 <1584731441@qq.com>
2021-03-27 17:18:03 +0800

    2021-3-27 增加了all.sql脚本

---
+++ doc/all.sql
@@ -0,0 +1,8 @@
+drop table if exists `test`;
+create table `test`(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    `password` varchar(50) comment '密码',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4
+comment='测试';
