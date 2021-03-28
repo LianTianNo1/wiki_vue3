22ee5f7a1e53d9cb328e8012883e232ac7549412
田浪 <1584731441@qq.com>
2021-03-28 20:31:15 +0800

    2021-3-28 集成Ant Design Vue,加入按钮示例

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <a-button type="danger">
+      Danger
+    </a-button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
