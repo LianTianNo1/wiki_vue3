78838792e1b38a2dd84ef550edaa9d5ecddc7a5a
田浪 <1584731441@qq.com>
2021-03-28 15:58:45 +0800

    2021-3-28 完成电子书列表查询接口

---
+++ src/main/java/com/wzy/wiki/controller/EbookController.java
@@ -0,0 +1,23 @@
+package com.wzy.wiki.controller;
+
+import com.wzy.wiki.domain.Ebook;
+import com.wzy.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list(){
+        return ebookService.list();
+    }
+}
---
+++ src/main/java/com/wzy/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.wzy.wiki.service;
+
+import com.wzy.wiki.domain.Ebook;
+import com.wzy.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list(){
+        return ebookMapper.selectByExample(null);
+    }
+}
