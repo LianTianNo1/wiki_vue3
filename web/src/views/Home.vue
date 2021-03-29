4cb69e64f4f94f55dd71800598c32e962acc9151
田浪 <1584731441@qq.com>
2021-03-29 16:01:16 +0800

    2021-3-29 加入Ant Design Vue list组件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -46,10 +46,38 @@
     <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-{{ebooks}}
-{{ebooks2}}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                      width="272"
+                      alt="logo"
+                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
@@ -57,6 +85,20 @@
 <script lang="ts">
 import { defineComponent,onMounted,ref,reactive,toRef } from 'vue';
 import axios from 'axios';
+
+const listData: any = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+            'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
 
 export default defineComponent({
   name: 'Home',
@@ -77,7 +119,19 @@ export default defineComponent({
 
     return{
       ebooks,
-      ebooks2:toRef(ebook1,"books")
+      ebooks2:toRef(ebook1,"books"),
+      listData,
+      pagination:{
+        onChange: (page: any) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      action: [
+        { type: 'StarOutlined', text: '156' },
+        { type: 'LikeOutlined', text: '156' },
+        { type: 'MessageOutlined', text: '2' },
+      ],
     }
   }
 });
