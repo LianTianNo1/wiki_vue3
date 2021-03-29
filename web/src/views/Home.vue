2f5e337b43c94746b8d8341dc4d42674069feceb
田浪 <1584731441@qq.com>
2021-03-29 11:41:28 +0800

    2021-3-29 去掉HelloWorld组件相关代码

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -55,12 +55,8 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 });
 </script>
