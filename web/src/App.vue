1c93efc477e7a6997e5549657eccbb94abbb22ed
田浪 <1584731441@qq.com>
2021-03-29 11:38:31 +0800

    2021-3-29 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,20 @@
   <a-layout>
     <the-header></the-header>
      <router-view/>
-    <a-layout-footer style="text-align: center">
-      Wiki
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
   import TheHeader from '@/components/the-header.vue';
+  import TheFooter from '@/components/the-footer.vue';
 
   export default defineComponent({
     name: 'app',
     components:{
       TheHeader,
+      TheFooter
     }
   });
 </script>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <a-layout-footer style="text-align: center">
+        Wiki@wzy
+    </a-layout-footer>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'the-footer',
+    });
+</script>
