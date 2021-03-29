ace54eac92be5acb0a2dda8d8bcdb44c7c79b982
田浪 <1584731441@qq.com>
2021-03-29 14:41:41 +0800

    2021-3-29 使用axios调用后端接口/ebook/list

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -4,7 +4,6 @@
         <a-menu
                 theme="dark"
                 mode="horizontal"
-                v-model:selectedKeys="selectedKeys1"
                 :style="{ lineHeight: '64px' }"
         >
             <a-menu-item key="1">nav 1</a-menu-item>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -3,8 +3,6 @@
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
               mode="inline"
-              v-model:selectedKeys="selectedKeys2"
-              v-model:openKeys="openKeys"
               :style="{ height: '100%', borderRight: 0 }"
       >
         <a-sub-menu key="sub1">
@@ -55,8 +53,15 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
+  setup(){
+    console.log("setup");
+    axios.get("http://localhost:8080/ebook/list?name=spring").then(function (response) {
+      console.log(response);
+    })
+  }
 });
 </script>
