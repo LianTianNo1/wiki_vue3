b167dbf65045605635fee28bfb652b936e2abf73
田浪 <1584731441@qq.com>
2021-03-30 19:47:35 +0800

    2021-3-30 增加电子书管理页面布局

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>电子书管理页</h1>
-  </div>
+  <a-layout>
+    <a-layout-content
+            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <div class="about">
+        <h1>电子书管理页</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
