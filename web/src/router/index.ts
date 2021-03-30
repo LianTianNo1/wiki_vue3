745a21f639ee8d4a9c6dd940b34642b66820c58e
田浪 <1584731441@qq.com>
2021-03-30 19:31:57 +0800

    2021-3-30 将页面风格改为小写字母开头

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home.vue'
+import About from '../views/about.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,10 +11,11 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
+    component: About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
   }
 ]
 
--- web/src/views/about.vue
+++ web/src/views/about.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>关于我们</h1>
   </div>
 </template>
