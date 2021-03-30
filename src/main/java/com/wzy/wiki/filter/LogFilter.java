63fd2ce40a4898b59c65e2e30c160a94646cb110
田浪 <1584731441@qq.com>
2021-03-30 17:00:29 +0800

    2021-3-30 增加过滤器,打印接口耗时

---
+++ src/main/java/com/wzy/wiki/filter/LogFilter.java
@@ -0,0 +1,41 @@
+package com.wzy.wiki.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class LogFilter implements Filter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException{
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // 打印请求消息
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        LOG.info("---------------LogFilter 开始---------------");
+        LOG.info("请求地址: {} {}",request.getRequestURL().toString(),request.getMethod());
+        LOG.info("远程地址: {}",request.getRemoteAddr());
+
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest,servletResponse);
+        LOG.info("---------------LogFilter 结束 耗时: {} ms---------------",System.currentTimeMillis() - startTime);
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+
+}
