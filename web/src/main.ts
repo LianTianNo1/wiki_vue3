4bea0bed39bad1721b62eb6d45859a0c00046e88
田浪 <1584731441@qq.com>
2021-03-30 11:34:54 +0800

    2021-3-30 vue多环境配置，增加了dev和prod配置文件，编译和启动命令增加多环境

--- web/src/main.ts
+++ web/src/main.ts
@@ -14,3 +14,6 @@ const icons: any = Icons;
 for (const i in icons){
     app.component(i, icons[i]);
 }
+
+console.log('环境: ', process.env.NODE_ENV);
+console.log('服务端: ', process.env.VUE_APP_SERVER);
