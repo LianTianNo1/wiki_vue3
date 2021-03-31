2b0103a85fdffec6c16fdc9d09c30e1b6ad392db
田浪 <1584731441@qq.com>
2021-03-31 15:49:04 +0800

    2021-3-31 电子书管理页面前后端分页整合成功

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -39,7 +39,7 @@
       const ebooks = ref();
       const pagination = ref({
         current: 1,
-        pageSize: 2,
+        pageSize: 4,
         total: 0
       });
       const loading = ref(false);
@@ -86,13 +86,19 @@
        */
       const handleQuery = (params: any) => {
         loading.value = true;
-        axios.get("/ebook/list",params).then((response) => {
+        axios.get("/ebook/list",{
+          params: {
+            page: params.page,
+            size: params.size
+          }
+        }).then((response) => {
           loading.value = false;
           const data = response.data;
-          ebooks.value = data.content;
+          ebooks.value = data.content.list;
 
           //重置分页按钮
           pagination.value.current = params.page;
+          pagination.value.total = data.content.total;
         });
       };
 
@@ -108,7 +114,10 @@
       };
 
       onMounted(() => {
-        handleQuery({});
+        handleQuery({
+          page: 1,
+          size: pagination.value.pageSize
+        });
       });
 
       return {
