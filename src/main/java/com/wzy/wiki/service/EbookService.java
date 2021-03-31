f26bbe526ba8e60301411d0dd8c5f76dd6d1bb25
田浪 <1584731441@qq.com>
2021-03-31 10:44:00 +0800

    2021-3-31 集成PageHelper插件,将mapper日志级别改为trace,打印所有的sql日志

--- src/main/java/com/wzy/wiki/service/EbookService.java
+++ src/main/java/com/wzy/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.wzy.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.wzy.wiki.domain.Ebook;
 import com.wzy.wiki.domain.EbookExample;
 import com.wzy.wiki.mapper.EbookMapper;
 import com.wzy.wiki.req.EbookReq;
 import com.wzy.wiki.resp.EbookResp;
 import com.wzy.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,6 +18,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -24,7 +30,12 @@ public class EbookService {
         if(!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数: {}",pageInfo.getTotal());
+        LOG.info("总页数: {}",pageInfo.getPages());
 
         /*List<EbookResp> respList = new ArrayList<>();
         for (Ebook ebook : ebookList) {
