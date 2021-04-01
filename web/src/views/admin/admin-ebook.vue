736f4feef97ffaac46074658d93a933a394d7f50
田浪 <1584731441@qq.com>
2021-04-01 11:01:56 +0800

    2021-4-1 点击编辑按钮时，弹出模态框

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -16,7 +16,7 @@
         </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
-            <a-button type="primary">
+            <a-button type="primary" @click="edit">
               编辑
             </a-button>
             <a-button type="danger">
@@ -27,6 +27,15 @@
       </a-table>
     </a-layout-content>
   </a-layout>
+
+  <a-modal
+    title="电子书表单"
+    v-model:visible="modalVisible"
+    :confirm-loading="modalLoading"
+    @ok="handleModalOk"
+  >
+    <p>test</p>
+  </a-modal>
 </template>
 
 <script lang="ts">
@@ -113,6 +122,24 @@
         });
       };
 
+      //--------表单--------
+      const modalVisible = ref(false);
+      const modalLoading = ref(false);
+      const handleModalOk = () => {
+        modalLoading.value = true;
+        setTimeout(() => {
+          modalVisible.value = false;
+          modalLoading.value = false;
+        },800);
+      };
+
+      /**
+       * 编辑
+       */
+      const edit = () => {
+        modalVisible.value = true;
+      };
+
       onMounted(() => {
         handleQuery({
           page: 1,
@@ -125,7 +152,13 @@
         pagination,
         columns,
         loading,
-        handleTableChange
+        handleTableChange,
+
+        edit,
+
+        modalVisible,
+        modalLoading,
+        handleModalOk
       }
     }
   });
