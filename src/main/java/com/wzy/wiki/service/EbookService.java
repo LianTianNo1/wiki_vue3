1eedb657e86a28230839f8f4d6a4c38bd4ea5008
田浪 <1584731441@qq.com>
2021-04-02 11:06:24 +0800

    2021-4-2 增加了雪花算法工具类，完成电子书新增功能

--- src/main/java/com/wzy/wiki/service/EbookService.java
+++ src/main/java/com/wzy/wiki/service/EbookService.java
@@ -10,6 +10,7 @@ import com.wzy.wiki.req.EbookSaveReq;
 import com.wzy.wiki.resp.EbookQueryResp;
 import com.wzy.wiki.resp.PageResp;
 import com.wzy.wiki.util.CopyUtil;
+import com.wzy.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,9 @@ public class EbookService {
 
     @Resource
     private EbookMapper ebookMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
 
     public PageResp<EbookQueryResp> list(EbookQueryReq req){
         EbookExample ebookExample = new EbookExample();
@@ -65,6 +69,7 @@ public class EbookService {
         Ebook ebook = CopyUtil.copy(req,Ebook.class);
         if (ObjectUtils.isEmpty(req.getId())){
             //新增
+            ebook.setId(snowFlake.nextId());
             ebookMapper.insert(ebook);
         }else {
             //更新
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -3,6 +3,11 @@
     <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
+      <p>
+        <a-button type="primary" @click="add()" size="large">
+          新增
+        </a-button>
+      </p>
       <a-table
         :columns="columns"
         :row-key="record => record.id"
@@ -169,6 +174,14 @@
         ebook.value = record
       };
 
+      /**
+       * 新增
+       */
+      const add = () => {
+        modalVisible.value = true;
+        ebook.value = {};
+      };
+
       onMounted(() => {
         handleQuery({
           page: 1,
@@ -184,6 +197,7 @@
         handleTableChange,
 
         edit,
+        add,
 
         ebook,
         modalVisible,
