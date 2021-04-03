77f9e68e0c6ce2f028ca3ec32a3a35d1843dedb3
田浪 <1584731441@qq.com>
2021-04-03 20:57:00 +0800

    2021-4-3 优化,加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -256,6 +256,12 @@
             level1.value = [];
             level1.value = Tool.array2Tree(categorys,0);
             console.log("树形结构: ",level1.value);
+
+            //加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
+            handleQuery({
+              page: 1,
+              size: pagination.value.pageSize
+            });
           }else {
             message.error(data.message);
           }
@@ -274,10 +280,7 @@
 
       onMounted(() => {
         handleQueryCategory();
-        handleQuery({
-          page: 1,
-          size: pagination.value.pageSize
-        });
+
       });
 
       return {
