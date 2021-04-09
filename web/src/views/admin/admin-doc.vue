f881995120bf97d020db24de80fed8cfc41df8c4
田浪 <1584731441@qq.com>
2021-04-09 20:29:24 +0800

    2021-4-9 增加页面参数传递，完成新增文档功能

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -96,10 +96,12 @@
   import axios from 'axios';
   import { message } from 'ant-design-vue';
   import {Tool} from "@/util/tool";
+  import {useRoute} from "vue-router";
 
   export default defineComponent({
     name: 'AdminDoc',
     setup(){
+      const route = useRoute();
       const param = ref();
       param.value = {};
       const docs = ref();
@@ -240,7 +242,9 @@
        */
       const add = () => {
         modalVisible.value = true;
-        doc.value = {};
+        doc.value = {
+          ebookId: route.query.ebookId
+        };
 
         treeSelectData.value = Tool.copy(level1.value);
 
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -36,7 +36,7 @@
         </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
-            <router-link to="/admin/doc">
+            <router-link :to="'/admin/doc?ebookId=' + record.id">
               <a-button type="primary">
                 文档管理
               </a-button>
