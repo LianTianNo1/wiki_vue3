faf81f845858f2cfb6d122458d423625f36d96d0
田浪 <1584731441@qq.com>
2021-04-11 15:06:05 +0800

    2021-4-11 文档管理布局美化 表格紧凑 按钮变小 表单变大 页面打开时就初始化富文本，zIndex变小

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -3,7 +3,7 @@
     <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-row>
+      <a-row :gutter="24">
         <a-col :span="8">
           <p>
             <a-form layout="inline" :model="param">
@@ -25,13 +25,14 @@
                   :data-source="level1"
                   :loading="loading"
                   :pagination="false"
+                  size="small"
           >
-            <template #cover="{ text: cover }">
-              <img v-if="cover" :src="cover" alt="avatar"/>
+            <template #name="{ text, record }">
+              {{record.sort}} {{text}}
             </template>
             <template v-slot:action="{ text, record }">
               <a-space size="small">
-                <a-button type="primary" @click="edit(record)">
+                <a-button type="primary" @click="edit(record)" size="small">
                   编辑
                 </a-button>
                 <a-popconfirm
@@ -41,7 +42,7 @@
                         @confirm="handleDelete(record.id)"
                 >
 
-                  <a-button type="danger">
+                  <a-button type="danger" size="small">
                     删除
                   </a-button>
                 </a-popconfirm>
@@ -51,11 +52,20 @@
           </a-table>
         </a-col>
         <a-col :span="16">
-          <a-form :model="doc" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
-            <a-form-item label="名称">
-              <a-input v-model:value="doc.name"/>
+          <p>
+            <a-form layout="inline" :modal="param">
+              <a-form-item>
+                <a-button type="primary" @click="handleSave()">
+                  保存
+                </a-button>
+              </a-form-item>
+            </a-form>
+          </p>
+          <a-form :model="doc" layout="vertical">
+            <a-form-item>
+              <a-input v-model:value="doc.name" placeholder="名称"/>
             </a-form-item>
-            <a-form-item label="父文档">
+            <a-form-item>
               <a-tree-select
                       v-model:value="doc.parent"
                       style="width: 100%"
@@ -67,10 +77,10 @@
               >
               </a-tree-select>
             </a-form-item>
-            <a-form-item label="顺序">
-              <a-input v-model:value="doc.sort"/>
+            <a-form-item>
+              <a-input v-model:value="doc.sort" placeholder="顺序"/>
             </a-form-item>
-            <a-form-item label="内容">
+            <a-form-item>
               <div id="content"></div>
             </a-form-item>
           </a-form>
@@ -111,16 +121,8 @@ import E from 'wangeditor';
       const columns = [
         {
           title: '名称',
-          dataIndex: 'name'
-        },
-        {
-          title: '父文档',
-          key: 'parent',
-          dataIndex: 'parent'
-        },
-        {
-          title: '顺序',
-          dataIndex: 'sort'
+          dataIndex: 'name',
+          slots: { customRender: 'name' }
         },
         {
           title: 'Action',
@@ -175,8 +177,9 @@ import E from 'wangeditor';
       const modalVisible = ref(false);
       const modalLoading = ref(false);
       const editor = new E('#content');
+      editor.config.zIndex = 0;
 
-      const handleModalOk = () => {
+      const handleSave = () => {
         modalLoading.value = true;
 
         axios.post("/doc/save",doc.value).then((response) => {
@@ -273,9 +276,6 @@ import E from 'wangeditor';
 
         //为选择树添加一个 “无” 字
         treeSelectData.value.unshift({id: 0, name: '无'});
-        setTimeout(function () {
-          editor.create();
-        },100);
       };
 
       /**
@@ -291,9 +291,6 @@ import E from 'wangeditor';
 
         //为选择树添加一个 "无" 字
         treeSelectData.value.unshift({id: 0, name: "无"});
-        setTimeout(function () {
-          editor.create();
-        },100);
       };
 
 
@@ -331,6 +328,8 @@ import E from 'wangeditor';
 
       onMounted(() => {
         handleQuery();
+
+        editor.create();
       });
 
       return {
@@ -348,7 +347,7 @@ import E from 'wangeditor';
         doc,
         modalVisible,
         modalLoading,
-        handleModalOk,
+        handleSave,
 
         treeSelectData
       }
