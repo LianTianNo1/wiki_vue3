9cf810f70a561beb0c9249e3401ad77e1195f011
田浪 <1584731441@qq.com>
2021-04-12 20:29:06 +0800

    2021-4-12 BUG修复: 打开编辑页面时，应该初始化父文档选择框，免去手动点新增

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -119,6 +119,9 @@ import E from 'wangeditor';
       param.value = {};
       const docs = ref();
       const loading = ref(false);
+      //因为树选择组件的属性状态会随着当前编辑的节点而变化，所以单独声明一个响应式变量
+      const treeSelectData = ref();
+      treeSelectData.value = [];
 
       const columns = [
         {
@@ -166,6 +169,11 @@ import E from 'wangeditor';
           level1.value = [];
           level1.value = Tool.array2Tree(docs.value,0);
           console.log("树形结构: ",level1);
+
+          //父文档下拉框初始化，相当于点击新增
+          treeSelectData.value = Tool.copy(level1.value);
+          // 为选择树添加一个 “无” 字
+            treeSelectData.value.unshift({id: 0, name: '无'});
           }else {
             message.error(data.message);
           }
@@ -173,9 +181,6 @@ import E from 'wangeditor';
       };
 
       //--------表单--------
-      //因为树选择组件的属性状态会随着当前编辑的节点而变化，所以单独声明一个响应式变量
-      const treeSelectData = ref();
-      treeSelectData.value = [];
       const doc = ref();
       doc.value = {};
       const modalVisible = ref(false);
