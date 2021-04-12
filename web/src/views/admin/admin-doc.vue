46ee45b88d14da2b9cd262536640fe0374240898
田浪 <1584731441@qq.com>
2021-04-12 21:12:22 +0800

    2021-4-12 文档管理增加内容预览功能

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -83,11 +83,20 @@
               <a-input v-model:value="doc.sort" placeholder="顺序"/>
             </a-form-item>
             <a-form-item>
+              <a-button type="primary" @click="handlePreviewContent()">
+                <EyeOutlined/>内容预览
+              </a-button>
+            </a-form-item>
+            <a-form-item>
               <div id="content"></div>
             </a-form-item>
           </a-form>
         </a-col>
       </a-row>
+
+      <a-drawer width="900" placement="right" :closable="false" :visible="drawerVisible" @close="onDrawerClose">
+        <div class="wangeditor" :innerHTML="previewHtml"></div>
+      </a-drawer>
 
     </a-layout-content>
   </a-layout>
@@ -353,6 +362,18 @@ import E from 'wangeditor';
 
       };
 
+      //------------富文本预览------------
+      const drawerVisible = ref(false);
+      const previewHtml = ref();
+      const handlePreviewContent = () => {
+        const html = editor.txt.html();
+        previewHtml.value = html;
+        drawerVisible.value = true;
+      };
+      const onDrawerClose = () => {
+        drawerVisible.value = false;
+      }
+
 
       onMounted(() => {
         handleQuery();
@@ -377,7 +398,12 @@ import E from 'wangeditor';
         modalLoading,
         handleSave,
 
-        treeSelectData
+        treeSelectData,
+
+        drawerVisible,
+        previewHtml,
+        handlePreviewContent,
+        onDrawerClose
       }
     }
   });
