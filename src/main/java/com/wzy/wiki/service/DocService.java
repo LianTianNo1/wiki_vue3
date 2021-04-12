a1dc7ab9e651f2d4edd323f32b434f164517be1d
田浪 <1584731441@qq.com>
2021-04-12 20:31:52 +0800

    2021-4-12 BUG修复: 如果文档内容表没数据，则查询内容表会出现空指针异常

--- src/main/java/com/wzy/wiki/service/DocService.java
+++ src/main/java/com/wzy/wiki/service/DocService.java
@@ -121,7 +121,11 @@ public class DocService {
 
     public String findContent(Long id){
         Content content = contentMapper.selectByPrimaryKey(id);
-        return content.getContent();
+        if (ObjectUtils.isEmpty(content)){
+            return "";
+        }else {
+            return content.getContent();
+        }
     }
 
 }
