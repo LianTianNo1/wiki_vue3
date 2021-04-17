bb851e42007a65362782ebc5c49f65aabafe48ba
田浪 <1584731441@qq.com>
2021-04-17 15:15:26 +0800

    2021-4-17 用户表设计，生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -94,3 +94,14 @@ create table `content` (
     `content` mediumtext not null comment '内容',
     primary key (`id`)
 ) engine=innodb default charset = utf8mb4 comment = '文档内容';
+
+-- 用户表
+drop table if exists `user`;
+create table `user` (
+                        `id` bigint not null comment 'ID',
+                        `login_name` varchar(50) not null comment '登陆名',
+                        `name` varchar(50) comment '昵称',
+                        `password` char(32) not null comment '密码',
+                        primary key (`id`),
+                        unique key `login_name_unique` (`login_name`)
+) engine=innodb default charset=utf8mb4 comment='用户';
---
+++ src/main/java/com/wzy/wiki/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package com.wzy.wiki.mapper;
+
+import com.wzy.wiki.domain.User;
+import com.wzy.wiki.domain.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
