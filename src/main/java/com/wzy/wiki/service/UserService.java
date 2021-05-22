dad365d37c658976e2d79b9a14415008126bcfd8
田浪 <1584731441@qq.com>
2021-05-22 16:28:47 +0800

    2021-5-22 修改用户信息的时候，用户名不允许被修改

--- src/main/java/com/wzy/wiki/service/UserService.java
+++ src/main/java/com/wzy/wiki/service/UserService.java
@@ -84,7 +84,8 @@ public class UserService {
 
         }else {
             //更新
-            userMapper.updateByPrimaryKey(user);
+            user.setLoginName(null);
+            userMapper.updateByPrimaryKeySelective(user);
         }
     }
 
