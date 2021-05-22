9e789404814999a5068d5d8639e29f2a5c3aae18
田浪 <1584731441@qq.com>
2021-05-22 17:49:31 +0800

    2021-5-22 密码加密存储与加密传输

--- src/main/java/com/wzy/wiki/controller/UserController.java
+++ src/main/java/com/wzy/wiki/controller/UserController.java
@@ -6,6 +6,7 @@ import com.wzy.wiki.resp.CommonResp;
 import com.wzy.wiki.resp.UserQueryResp;
 import com.wzy.wiki.resp.PageResp;
 import com.wzy.wiki.service.UserService;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -28,6 +29,7 @@ public class UserController {
 
     @PostMapping("/save")
     public CommonResp save(@Valid @RequestBody UserSaveReq req){
+        req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp resp = new CommonResp();
         userService.save(req);
         return resp;
