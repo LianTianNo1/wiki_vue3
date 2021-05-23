d7306aca8b3edb4f173aca35d8376a41c8355765
田浪 <1584731441@qq.com>
2021-05-23 15:04:22 +0800

    2021-5-23 集成redis，登录成功后将登录信息放入redis中

--- src/main/java/com/wzy/wiki/controller/UserController.java
+++ src/main/java/com/wzy/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.wzy.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wzy.wiki.req.UserLoginReq;
 import com.wzy.wiki.req.UserQueryReq;
 import com.wzy.wiki.req.UserResetPasswordReq;
@@ -9,18 +10,31 @@ import com.wzy.wiki.resp.PageResp;
 import com.wzy.wiki.resp.UserLoginResp;
 import com.wzy.wiki.resp.UserQueryResp;
 import com.wzy.wiki.service.UserService;
+import com.wzy.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     private UserService userService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req){
@@ -62,6 +76,13 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp();
         UserLoginResp userLoginResp = userService.login(req);
+
+        // 生成单点登录token，并放入redis中
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token: {} ，并放入redis中",token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
--- src/main/java/com/wzy/wiki/resp/UserLoginResp.java
+++ src/main/java/com/wzy/wiki/resp/UserLoginResp.java
@@ -7,6 +7,8 @@ public class UserLoginResp {
 
     private String name;
 
+    private String token;
+
     public Long getId() {
         return id;
     }
@@ -31,16 +33,21 @@ public class UserLoginResp {
         this.name = name;
     }
 
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", loginName=").append(loginName);
-        sb.append(", name=").append(name);
-        sb.append("]");
-        return sb.toString();
+        return "UserLoginResp{" +
+                "id=" + id +
+                ", loginName='" + loginName + '\'' +
+                ", name='" + name + '\'' +
+                ", token='" + token + '\'' +
+                '}';
     }
 }
