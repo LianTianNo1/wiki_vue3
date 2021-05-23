ba7e4c6f2198c938d01a9ee1d08e813a805d64b1
田浪 <1584731441@qq.com>
2021-05-23 10:28:00 +0800

    2021-5-23 前端增加登录模态框

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -21,14 +21,63 @@
             <a-menu-item key="About">
                 <router-link to="/about">关于我们</router-link>
             </a-menu-item>
+            <a class="login-menu" @click="showLoginModal">
+                <span>登录</span>
+            </a>
         </a-menu>
+        <a-modal
+                title="登录"
+                v-modal:visible="loginModalVisible"
+                :confirm-loading="loginModalLoading"
+                @ok="login"
+        >
+            <a-form :model="loginUser" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+                <a-form-item label="登录名">
+                    <a-input v-model:value="loginUser.loginName" />
+                </a-form-item>
+                <a-form-item label="密码">
+                    <a-input v-model:value="loginUser.password" type="password" />
+                </a-form-item>
+            </a-form>
+        </a-modal>
     </a-layout-header>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+    import { defineComponent, ref } from 'vue';
 
     export default defineComponent({
         name: 'the-header',
+        setup () {
+            const loginUser = ref({
+               loginName: "test",
+                password: "test"
+            });
+            const loginModalVisible = ref(false);
+            const loginModalLoading = ref(false);
+            const showLoginModal = () => {
+                loginModalVisible.value = true;
+            }
+
+            // 开始登录
+            const login = () => {
+                console.log("开始登录");
+            }
+
+            return {
+                loginModalVisible,
+                loginModalLoading,
+                showLoginModal,
+                loginUser,
+                login
+            }
+        }
     });
 </script>
+
+<style>
+    .login-menu{
+        float: right;
+        color: white;
+    }
+</style>
