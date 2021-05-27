99f2f9a4297de8599c5cf582b33b68beafaee251
田浪 <1584731441@qq.com>
2021-05-27 21:11:33 +0800

    2021-5-27 前端增加数值统计组件，显示统计数值

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -1,16 +1,130 @@
 <template>
-    <h1>欢迎使用Wiki知识库系统11111111111</h1>
+    <div>
+        <a-row>
+            <a-col :span="24">
+                <a-card>
+                    <a-row>
+                        <a-col :span="8">
+                            <a-statistic title="总阅读量" :value="statistic.viewCount">
+                                <template #suffix>
+                                    <UserOutlined/>
+                                </template>
+                            </a-statistic>
+                        </a-col>
+                        <a-col :span="8">
+                            <a-statistic title="总点赞量" :value="statistic.voteCount">
+                                <template #suffix>
+                                    <like-outlined/>
+                                </template>
+                            </a-statistic>
+                        </a-col>
+                        <a-col :span="8">
+                            <a-statistic title="点赞率" :value="statistic.voteCount / statistic.viewCount * 100"
+                                            :precision="2"
+                                            suffix="%"
+                                            :value-style="{ color: '#cf1322' }">
+                                <template #suffix>
+                                    <like-outlined/>
+                                </template>
+                            </a-statistic>
+                        </a-col>
+                    </a-row>
+                </a-card>
+            </a-col>
+        </a-row>
+        <br>
+        <a-row :gutter="16">
+            <a-col :span="12">
+                <a-card>
+                    <a-row>
+                        <a-col :span="12">
+                            <a-statistic title="今日阅读" :value="statistic.todayViewCount" style="margin-right: 50px">
+                                <template #suffix>
+                                    <UserOutlined/>
+                                </template>
+                            </a-statistic>
+                        </a-col>
+                        <a-col :span="12">
+                            <a-statistic title="今日点赞" :value="statistic.todayVoteCount">
+                                <template #suffix>
+                                    <like-outlined />
+                                </template>
+                            </a-statistic>
+                        </a-col>
+                    </a-row>
+                </a-card>
+            </a-col>
+            <a-col :span="12">
+                <a-card>
+                    <a-row>
+                        <a-col :span="12">
+                            <a-statistic
+                                    title="预计今日阅读"
+                                    :value="statistic.todayViewIncrease"
+                                    :value-style="{ color: '#0000ff' }"
+                            >
+                                <template #suffix>
+                                    <UserOutlined />
+                                </template>
+                            </a-statistic>
+                        </a-col>
+                        <a-col :span="12">
+                            <a-statistic
+                                    title="预计今日阅读增长"
+                                    :value="statistic.todayViewIncreaseRateAbs"
+                                    :precision="2"
+                                    suffix="%"
+                                    class="demo-class"
+                                    :value-style="statistic.todayViewIncreaseRate < 0 ? { color: '#3f8600' } : { color: '#cf1322' }"
+                            >
+                                <template #prefix>
+                                    <arrow-down-outlined v-if="statistic.todayViewIncreaseRate < 0" />
+                                    <arrow-up-outlined v-if="statistic.todayViewIncreaseRate >= 0" />
+                                </template>
+                            </a-statistic>
+                        </a-col>
+                    </a-row>
+                </a-card>
+            </a-col>
+        </a-row>
+    </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+    import { defineComponent, ref, onMounted } from 'vue';
+    import axios from 'axios';
 
     export default defineComponent({
         name: 'the-welcome',
         setup () {
+            const statistic = ref();
+            statistic.value = {};
+            const getStatistic = () => {
+                axios.get('/ebook-snapshot/get-statistic').then((response) => {
+                    const data = response.data;
+                    if (data.success) {
+                        const statisticResp = data.content;
+                        statistic.value.viewCount = statisticResp[1].viewCount;
+                        statistic.value.voteCount = statisticResp[1].voteCount;
+                        statistic.value.todayViewCount = statisticResp[1].viewIncrease;
+                        statistic.value.todayVoteCount = statisticResp[1].voteIncrease;
+
+                        // 按分钟计算当前时间点，占一天的百分比
+                        const now = new Date();
+                        const nowRate = (now.getHours() * 60 + now.getMinutes()) / (60 * 24);
+                        statistic.value.todayViewIncrease = parseInt(String(statisticResp[1].viewIncrease / nowRate));
+                        statistic.value.todayViewIncreaseRate = (statistic.value.todayViewIncrease - statisticResp[0].viewIncrease) / statisticResp[0].viewIncrease * 100;
+                        statistic.value.todayViewIncreaseRateAbs = Math.abs(statistic.value.todayViewIncreaseRate);
+                    }
+                });
+            }
+
+            onMounted(() => {
+                getStatistic();
+            });
 
             return {
-
+                statistic
             }
         }
     });
