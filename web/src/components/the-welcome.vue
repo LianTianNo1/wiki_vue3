f7feb688d87c069ae5bc1b2d8028ded8e826fe62
田浪 <1584731441@qq.com>
2021-05-27 17:44:12 +0800

    2021-5-27 将欢迎界面提取成组件

---
+++ web/src/components/the-welcome.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>欢迎使用Wiki知识库系统11111111111</h1>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'the-welcome',
+        setup () {
+
+            return {
+
+            }
+        }
+    });
+</script>
--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -30,7 +30,7 @@
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <div class="welcome" v-show="isShowWelcome">
-        <h1>欢迎使用Wiki知识库系统</h1>
+        <the-welcome></the-welcome>
       </div>
       <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }"  :data-source="ebooks">
         <template #renderItem="{ item }">
@@ -69,6 +69,7 @@ import { defineComponent,onMounted,ref,reactive,toRef } from 'vue';
 import axios from 'axios';
 import { message } from "ant-design-vue";
 import { Tool } from "@/util/tool";
+import TheWelcome from '@/components/the-welcome.vue';
 
 /*const listData: any = [];
 for (let i = 0; i < 23; i++) {
@@ -86,6 +87,9 @@ for (let i = 0; i < 23; i++) {
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    TheWelcome
+  },
   setup(){
     const ebooks = ref();
     // const ebook1 = reactive({books:[]})
