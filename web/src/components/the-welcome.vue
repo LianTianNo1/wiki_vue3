0e3837113444c7a471449f2b8ffd29986dd365fa
田浪 <1584731441@qq.com>
2021-05-27 21:32:52 +0800

    2021-5-27 集成echarts5.1.1,增加示例

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -87,12 +87,20 @@
                 </a-card>
             </a-col>
         </a-row>
+        <br>
+        <a-row>
+            <a-col :span="24">
+                <div id="main" style="width: 100%; height: 300px"></div>
+            </a-col>
+        </a-row>
     </div>
 </template>
 
 <script lang="ts">
     import { defineComponent, ref, onMounted } from 'vue';
     import axios from 'axios';
+
+    declare let echarts: any;
 
     export default defineComponent({
         name: 'the-welcome',
@@ -117,10 +125,38 @@
                         statistic.value.todayViewIncreaseRateAbs = Math.abs(statistic.value.todayViewIncreaseRate);
                     }
                 });
+            };
+
+            const testEcharts = () => {
+                // 基于准备好的dom，初始化echarts实例
+                const myChart = echarts.init(document.getElementById('main'));
+
+                // 指定图表的配置项和数据
+                const option = {
+                    title: {
+                        text: 'ECharts 入门示例'
+                    },
+                    tooltip: {},
+                    legend: {
+                        data:['销量']
+                    },
+                    xAxis: {
+                        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '销量',
+                        type: 'bar',
+                        data: [5, 20, 36, 10, 10, 20]
+                    }]
+                };
+                // 使用刚指定的配置项和数据显示图表。
+                myChart.setOption(option);
             }
 
             onMounted(() => {
                 getStatistic();
+                testEcharts();
             });
 
             return {
